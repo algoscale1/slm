4e15c00c6873266ebec9b7e2ebde78b567ba99e2
NEERAJ YADAV <nkyadav@algoscale.com>
2017-02-27 14:46:39 +0530

    graph database implemented

--- slm/app/models.py
+++ slm/app/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
 # Create your models here.
+
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, config
+
+config.DATABASE_URL = 'bolt://neo4j:neo4j@localhost:7687'
+
+
+class Term(StructuredNode):
+    name = StringProperty(unique_index=True)
+    header = RelationshipTo('Header', 'DEFINED_IN')
+
+
+class Header(StructuredNode):
+    title = StringProperty(unique_index=True)
+    terms = RelationshipFrom('Term', 'DEFINED_IN')
--- slm/app/views.py
+++ slm/app/views.py
@@ -8,6 +8,9 @@ from slm.app.tree_structures.act_and_regulations import cbcr, osact, cbcact
 from slm.app.tree_structures.forms import form1, form5, form6
 from slm.app.tree_structures.manual_and_notices import notice_cbc, notice_tsx, tsx_manual
 from slm.app.tree_structures.nis import ni58, ni51, ni54
+from slm.app.models import Header, Term
+import csv
+import pandas as pd
 
 tree = building_tree.BuildTree()
 score = score_calculator.ScoreData()
@@ -22,11 +25,13 @@ def get_answers(request):
     """
     requested_question = request.POST['question']
 
+    # construction of trees from data
     cbcr_tree, osact_tree, form1_tree, ni58_tree, notice_tsx_tree, notice_cbc_tree, ni51_tree, cbcact_tree, \
     ni54_tree, form5_tree, form6_tree, tsx_manual_tree = \
         tree.display_tree(cbcr, osact, form1, ni58, notice_tsx, notice_cbc, ni51, cbcact, ni54, form5, form6,
                           tsx_manual)
 
+    # Converting trees to data frame
     complete_data = score.converting_trees_to_df(cbcr_tree, osact_tree, form1_tree, ni58_tree, notice_tsx_tree,
                                             notice_cbc_tree,
                                             ni51_tree, cbcact_tree, ni54_tree, form5_tree, form6_tree,
@@ -47,9 +52,20 @@ def get_answers(request):
 @csrf_exempt
 def home(request):
     """
-    returns home page for quering questions
+    returns home page for querying questions
     :param request:
     :return:
     """
     return render(request, template_name="searchPage.html")
 
+
+@csrf_exempt
+def db(request):
+
+    df = pd.read_csv('/home/neeraj/Downloads/def.csv')
+    for index,row in df.iterrows():
+
+        executive_officer = Term(name=row['definition term']).save()
+        continuous_disclosure = Header(title=row['heading']).save()
+        executive_officer.header.connect(continuous_disclosure)
+    return HttpResponse("done")
